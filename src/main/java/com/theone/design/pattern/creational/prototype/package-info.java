848ffce0bcd3debf88aee21ad6a7497cf3556dcd
theone <281541534@qq.com>
2020-06-04 15:22:48 +0800

    修改了注释

--- src/main/java/com/theone/design/pattern/creational/prototype/package-info.java
+++ src/main/java/com/theone/design/pattern/creational/prototype/package-info.java
@@ -2,7 +2,7 @@
  * @Author: liuyu
  * @DateTime: 2020/4/14 17:29
  * @Description: 原型模式
- * 主要体现就是视频Cloneable接口，重写clone方法。
+ * 主要体现就是实现Cloneable接口，重写clone方法。
  * 应用层使用时无须每次重新创建对象
  */
 package com.theone.design.pattern.creational.prototype;
